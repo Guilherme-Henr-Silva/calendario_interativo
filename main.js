e7c340a2436098ad82df8ec01fa73a7dcb7a859f
Guilherme Silva <gui.ghs.silva@gmail.com>
2025-03-28 22:01:25 -0300

    Criando os blocos de variaveis (escopo de função

---
+++ main.js
@@ -0,0 +1,10 @@
+// Código para tornar calendário interativo, utilizando declaração interativa
+function colorirDia(){
+    // Bloco de variáveis (escopo de função)
+    let days = document.getElementById('day').value;
+    let color = document.getElementById('color').value;
+    let calendar = document.getElementById('calendar');
+
+    // Bloco de validação de informações
+    
+}
