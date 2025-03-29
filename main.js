3fa9e779cb26a7d974959ad71550c970db8d6655
Guilherme Silva <gui.ghs.silva@gmail.com>
2025-03-28 22:11:06 -0300

    Adicionando bloco de validação

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 // Código para tornar calendário interativo, utilizando declaração interativa
-function colorirDia(){
+function colorirDia() {
     // Bloco de variáveis (escopo de função)
     let days = document.getElementById('day').value;
     let color = document.getElementById('color').value;
     let calendar = document.getElementById('calendar');
 
     // Bloco de validação de informações
-    
+    if (!days) {
+        alert("Favor informar um dia exitente no calendário")
+    }
+    else {
+        if ((days > 0) && (days < 31)) {
+            let td = calendar.getElementsByTagName('td');
+            td.style.backgroundColor = color;
+        }
+        else {
+            alert("Favor informar uma data constante no calendário");
+        }
+    }
+
+    var elementos = document.querySelectorAll('td');
 }
